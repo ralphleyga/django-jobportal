fb12c184d3d1c5a27de35062cc5a05e5f8515e53
ralph <ralphfleyga@gmail.com>
2020-04-17 00:04:50 +0800

    add login design

--- work/users/views.py
+++ work/users/views.py
@@ -6,6 +6,12 @@ from jobs.models import Job
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get(self, request):
+        context = {
+            'jobs': Job.objects.filter(archived=False, draft=False).order_by('-created')[:3]
+        }
+        return self.render_to_response(context)
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
